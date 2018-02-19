42be8813e7f0a9a44f366921166eaabfd591771d
Yarne Coppens <yarne.coppens@student.howest.be>
2018-02-19 15:09:59 +0100

    Empty .NET web app

---
+++ Week2_Oefening1/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week2_Oefening1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
